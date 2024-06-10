be99ce1960c7a949b614df3ca88e4e43867cdeb2
zaeemtahir <zaeemtahir003@gmail.com>
2024-06-11 03:50:51 +0500

    payment key

--- Backend/Controllers/paymentController.js
+++ Backend/Controllers/paymentController.js
@@ -1,7 +1,6 @@
 import stripe from 'stripe'
 
-const stripeSecretKey =
-  'sk_test_51PJTtkGUY0qZiMwJ4E2rdBdbYZCB9atAIekQ5LkVATpPEktivpkUY7XQ8nBgMmokPCvF4qAgDF8Ul7imcsxMsHuM00IwMKSSHJ'
+const stripeSecretKey = process.env.STRIPE_SECRET_KEY
 const stripeInstance = stripe(stripeSecretKey)
 
 export const stripePayment = async (req, res) => {
